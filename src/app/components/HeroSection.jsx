95a1c931aee8c4f31fbae2c5cd9824555589fdcd
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-12 19:33:28 +0300

    header

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
     <section>
-      <h1 className='text-white' >Portfolio Website</h1>
+      <div className='grid grid-cols-1 lg:grid-cols-12'>
+        <div className='col-span-7 place-self-center'>
+          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold' >Hello, I'm Emirhan</h1>
+          <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, omnis.
+          </p>
+        </div>
+        <div className='col-span-5'>
+          <Image
+            src="/images/hero-image.png"
+            alt="hero-image"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+      
     </section>)
 }
 
