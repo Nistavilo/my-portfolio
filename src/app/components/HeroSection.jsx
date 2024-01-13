5e8f9b0325bd1d1d1931c5a7899e1fa4019e9f8c
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-13 12:25:13 +0300

    headerupdate

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,13 @@
+"use client";
 import React from 'react'
+
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+
+
+
+
+
 
 const HeroSection = () => {
   return (
@@ -9,7 +17,21 @@ const HeroSection = () => {
         <div className='col-span-7 place-self-center text-center sm:text-left'>
 
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold' >
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I'm {""} </span> Emirhan
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I'm {""} </span> 
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Emirhan',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Web Developer',
+                1000
+              ]}
+              wrapper="span"
+              speed={30}
+              style={{ fontSize: '2em', display: 'inline-block' }}
+              repeat={Infinity}
+            />
           </h1>
 
           <p className='text-[#ADB7BE] text-base text-lg mb-6 lg:text-xl'>
@@ -21,6 +43,7 @@ const HeroSection = () => {
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#42b883] via-[#00d0f6] to-[#d6002f] hover:bg-slate-200 text-white' >Hire Me</button>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800  mt-3' >
                <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Dowload CV</span>
+
             </button>
           </div>
         </div>
