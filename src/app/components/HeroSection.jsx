f1a7342a6ff3d174ffae4ed302215384776b12a0
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-13 23:04:11 +0300

    navbar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -34,13 +34,13 @@ const HeroSection = () => {
             />
           </h1>
 
-          <p className='text-[#ADB7BE] text-base text-lg mb-6 lg:text-xl'>
+          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
 
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, omnis.
 
           </p>
           <div>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#42b883] via-[#00d0f6] to-[#d6002f] hover:bg-slate-200 text-white' >Hire Me</button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white' >Hire Me</button>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800  mt-3' >
                <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Dowload CV</span>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col bg-[#121212] '>
-      <div class="container mx-auto px-12 py-4"></div>
+      <Navbar/>
+      <div class="container mt-24 mx-auto px-12 py-4"></div>
       <HeroSection/>
     </main>
   )
