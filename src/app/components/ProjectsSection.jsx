f7c2756ffa7508ec13b4d0bf27231d32b9bf7e4d
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-16 22:41:58 +0300

    projectsSection

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 const ProjectsSection = () => {
   return (
-     <><h2>My Projects</h2> <div></div></>
+     <><h2>My Projects</h2> 
+     <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
+      {projectsData.map((project) => (
+        <ProjectCard 
+        key={project.id}
+         title = {project.title }
+          description={project.description} 
+          imgUrl={project.image}
+          />
+          ))}
+     </div>
+     </>
   )
 }
 
