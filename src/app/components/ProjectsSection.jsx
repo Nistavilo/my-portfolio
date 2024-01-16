1091704d9c61c7aa02fee0590cf28944d40cd118
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-16 21:35:27 +0300

    commit

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectsSection = () => {
+  return (
+     <><h2>My Projects</h2> <div></div></>
+  )
+}
+
+export default ProjectsSection
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -5,7 +5,7 @@ const TabButton = (active,selectTab,children) => {
 const buttonClasses = active ? 'text-white border-b border-purple-500' : 'text-[#ADB7BE] border-b border-purple-500'
     return (
     <button onClick={selectTab}>
-      <p className={'mr-3 font-semibold hover:text-white ${buttonClasses} '} ></p>
+      <p className='mr-3 font-semibold hover:text-white ${buttonClasses}'> </p>
         {children}
     </button>
 
