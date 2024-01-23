4be537e1f2880565458ff5fa589dba36c6c7d2c0
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-23 22:56:55 +0300

    com

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,20 +16,16 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Emirhan",
                 1000,
                 "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
+                1000 
               ]}
               wrapper="span"
               speed={50}
@@ -49,13 +45,13 @@ const HeroSection = () => {
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500   hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
             </Link>
-          </div>
+                      </div>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
