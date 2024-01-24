edbf47e32e4fa9c7f157ab7ef37b960e6cf876ee
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-24 21:30:31 +0300

    commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -47,10 +47,7 @@ const AboutSection = () => {
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
           I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express.js, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing websites.
+            interactive and responsive web applications.
           </p>
           
           <div className="flex flex-row justify-start mt-8">
