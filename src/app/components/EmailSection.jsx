5c5b9af7d3d0f39d93ce099dc0350c73d5f32a16
nistavilo <emirhankorkmaz275@gmail.com>
2024-01-25 20:20:56 +0300

    commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,83 +1,75 @@
-"use client";
+// Import necessary modules and libraries
 import React, { useState } from "react";
+import axios from "axios";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import Link from "next/link";
 import Image from "next/image";
 
+// EmailSection component
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
+  // Handle form submission using Axios
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    // Extract form data
     const data = {
       email: e.target.email.value,
       subject: e.target.subject.value,
       message: e.target.message.value,
     };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
+    try {
+      // Send POST request using Axios
+      const response = await axios.post("/api/send", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
+      // Check if the request was successful
+      if (response.status === 200) {
+        console.log("Message sent.");
+        setEmailSubmitted(true);
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
     }
   };
 
   return (
-    <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-    >
+    <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+      {/* Background circle */}
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      
+      {/* Contact information */}
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
-        </h5>
+        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+          I&apos;m currently looking for new opportunities. My inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
         </p>
+        
+        {/* Social icons */}
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
             <Image src={GithubIcon} alt="Github Icon" />
-          </Link>
-          <Link href="linkedin.com">
+          </a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>
+          </a>
         </div>
       </div>
+      
+      {/* Email form */}
       <div>
         {emailSubmitted ? (
-          <p className="text-green-500 text-sm mt-2">
-            Email sent successfully!
-          </p>
+          <p className="text-green-500 text-sm mt-2">Email sent successfully!</p>
         ) : (
           <form className="flex flex-col" onSubmit={handleSubmit}>
+            {/* Email input */}
             <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="text-white block mb-2 text-sm font-medium"
-              >
-                Your email
-              </label>
+              <label htmlFor="email" className="text-white block mb-2 text-sm font-medium">Your email</label>
               <input
                 name="email"
                 type="email"
@@ -87,13 +79,10 @@ const EmailSection = () => {
                 placeholder="email"
               />
             </div>
+            
+            {/* Subject input */}   
             <div className="mb-6">
-              <label
-                htmlFor="subject"
-                className="text-white block text-sm mb-2 font-medium"
-              >
-                Subject
-              </label>
+              <label htmlFor="subject" className="text-white block text-sm mb-2 font-medium">Subject</label>
               <input
                 name="subject"
                 type="text"
@@ -103,13 +92,10 @@ const EmailSection = () => {
                 placeholder="Just saying hi"
               />
             </div>
+            
+            {/* Message textarea */}
             <div className="mb-6">
-              <label
-                htmlFor="message"
-                className="text-white block text-sm mb-2 font-medium"
-              >
-                Message
-              </label>
+              <label htmlFor="message" className="text-white block text-sm mb-2 font-medium">Message</label>
               <textarea
                 name="message"
                 id="message"
@@ -117,6 +103,8 @@ const EmailSection = () => {
                 placeholder="Let's talk about..."
               />
             </div>
+            
+            {/* Submit button */}
             <button
               type="submit"
               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
